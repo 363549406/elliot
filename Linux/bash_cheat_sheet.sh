ebdb3c39e465f7e2832bd9e59681a908b0a53cf1
snail_mu <363549406@qq.com>
2018-09-16 11:52:15 +0800

    add bash cheat sheet

---
+++ Linux/bash_cheat_sheet.sh
@@ -0,0 +1,35 @@
+!#/bin/bash
+
+# SHORTCUTS
+
+CTRL+A # move to the beginning of line
+CTRL+B # moves backward one character
+CTRL+C # halts the current command
+CTRL+D # deletes one character backward or logs out of current session, similar to exit
+CTRL+E # moves to end of line
+CTRL+F # moves forward one character
+CTRL+G # aborts the current editing command and ring the termianl bell
+CTRL+J # same as RETURN
+CTRL+K # deletes (kill) forward to end of line
+CTRL+L # clears screen and redisplay the line
+CTRL+M # same as RETURN
+CTRL+N # next line in command history
+CTRL+O # same as history, then displays next line in history file
+CTRL+P # previous line in command history
+CTRL+R # searches backward
+CTRL+S # searches forward
+CTRL+T # transposes two characters
+CTRL+U # kills backward from points to the beginning of line
+CTRL+V # makes the next character typed verbatim 好比ctrl+v后再按ctrl+a会显示^A字符
+CTRL+W # kills the word behind the cursor
+CTRL+X # lists the possible filename completiions of the current word
+CTRL+Y # retrieves (yank) last item killed
+CTRL+Z # stop the current command, resume with fg in the foreground or bg in the background
+
+ALT+B  # moves backward one word
+ALT+D  # deletes next word
+ALT+F  # moves forward one word
+
+DELETE # deletes one character backward
+!!     # repeates the last command
+exit   # logs out of current session
