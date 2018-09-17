9b8be7ed81a596c43f29a99eee70068a28892b09
snail_mu <363549406@qq.com>
2018-09-17 18:45:45 +0800

    change order in bash_cheat_sheet

--- Linux/bash_cheat_sheet.sh
+++ Linux/bash_cheat_sheet.sh
@@ -2,33 +2,41 @@
 
 # SHORTCUTS
 
-CTRL+A # move to the beginning of line
 CTRL+B # moves backward one character
-CTRL+C # halts the current command
-CTRL+D # deletes one character backward or logs out of current session, similar to exit
-CTRL+E # moves to end of line
 CTRL+F # moves forward one character
-CTRL+G # aborts the current editing command and ring the termianl bell
-CTRL+J # same as RETURN
-CTRL+K # deletes (kill) forward to end of line
-CTRL+L # clears screen and redisplay the line
-CTRL+M # same as RETURN
-CTRL+N # next line in command history
-CTRL+O # same as history, then displays next line in history file
-CTRL+P # previous line in command history
-CTRL+R # searches backward
-CTRL+S # searches forward
-CTRL+T # transposes two characters
-CTRL+U # kills backward from points to the beginning of line
-CTRL+V # makes the next character typed verbatim 好比ctrl+v后再按ctrl+a会显示^A字符
-CTRL+W # kills the word behind the cursor
-CTRL+X # lists the possible filename completiions of the current word
-CTRL+Y # retrieves (yank) last item killed
-CTRL+Z # stop the current command, resume with fg in the foreground or bg in the background
+CTRL+A # moves to begin of line
+CTRL+E # moves to end of line
 
 ALT+B  # moves backward one word
-ALT+D  # deletes next word
 ALT+F  # moves forward one word
+
+CTRL+D # deletes one character backward or logs out of current session, similar to exit
+CTRL+W # kills the word behind the cursor
+CTRL+K # deletes (kill) forward to end of line
+CTRL+U # kills backward from points to the beginning of line
+
+ALT+D  # deletes next word
+
+CTRL+Y # retrieves (yank) last item killed
+CTRL+T # transposes two characters
+
+CTRL+P # previous line in command history
+CTRL+N # next line in command history
+CTRL+C # halts the current command
+
+CTRL+L # clears screen and redisplay the line
+
+CTRL+R # searches backward
+CTRL+S # searches forward
+
+CTRL+G # aborts the current editing command and ring the termianl bell
+CTRL+J # same as RETURN
+CTRL+M # same as RETURN
+CTRL+O # same as history, then displays next line in history file
+CTRL+V # makes the next character typed verbatim 好比ctrl+v后再按ctrl+a会显示^A字符
+CTRL+X # lists the possible filename completiions of the current word
+CTRL+Z # stop the current command, resume with fg in the foreground or bg in the background
+
 
 DELETE # deletes one character backward
 !!     # repeates the last command
